22a3090b8f38b82ab37584d507aa4fd00c300705
danteali <ryan@mc-guinness.co.uk>
2019-06-28 00:45:30 +0100

    Latest Update

--- _update_git.sh
+++ _update_git.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Check running as root 
-if [ $(id -u) -ne 0 ]; then tput setaf 1; echo "Not running as root, attempting to automatically restart script with root access..."; tput sgr0; echo; sudo $0 $*; exit 1; fi
-
 cd /home/ryan/scripts/docker
 
 git add .
