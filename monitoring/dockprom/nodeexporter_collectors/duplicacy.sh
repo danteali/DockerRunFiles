3b7a4553281180516ad248f72a9af00c3073ebc6
danteali <ryan@mc-guinness.co.uk>
2019-06-30 20:33:23 +0100

    Latest Update

---
+++ monitoring/dockprom/nodeexporter_collectors/duplicacy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Generate text file for node exporter collector to record duplicacy activity.
+# Call with arguments:
+# $1 = action, $2 = storage, $3=1(start)/0(stop)
+
+# Specify where output file should be saved. This should be where nodeexporter looks for the files.
+OUTPUTFILE=/storage/Docker/dockprom/nodeexporter/textfile_collector/duplicacy.prom
+
+# Define variables
+ACTION=$1
+STORAGE=$2
+ONOFF=$3
+
+echo "node_duplicacy{action=\"$ACTION\", storage=\"$STORAGE\"} $ONOFF" > $OUTPUTFILE
