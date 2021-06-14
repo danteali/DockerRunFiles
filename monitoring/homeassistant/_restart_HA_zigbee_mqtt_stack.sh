c0de118235ab08bf7d09b961b7852cf7f0db31f7
danteali <ryan@mc-guinness.co.uk>
2021-06-14 08:07:12 +0100

    Latest Update

--- monitoring/homeassistant/_restart_HA_zigbee_mqtt_stack.sh
+++ monitoring/homeassistant/_restart_HA_zigbee_mqtt_stack.sh
@@ -3,4 +3,4 @@
 /home/ryan/scripts/docker/monitoring/homeassistant/eclipse-mosquitto.start
 /home/ryan/scripts/docker/monitoring/homeassistant/zigbee2mqtt.start
 /home/ryan/scripts/docker/monitoring/homeassistant/node-red.start
-/home/ryan/scripts/docker/monitoring/homeassistant/4_update_ha_config_check_git_restart.sh
+/home/ryan/scripts/docker/monitoring/homeassistant/4_update_ha_config_check_git_restart_container.sh
